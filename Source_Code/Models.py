800e8f7f6bcfd8213004ac3b74bc92504a06fc6c
RWMostert <rwmmostert@gmail.com>
2017-08-07 18:24:34 +0000

    updated history, predictions and model

--- Source_Code/Models.py
+++ Source_Code/Models.py
@@ -123,6 +123,58 @@ def covnet_multiple_ELUs_8_layers(r_squared = True):
     
     return convnet_mo_elu
 
+def covnet_multiple_ELUs_10_layers(r_squared = True):
+    """
+    returns an 8-layer convolutional architecture (with ELU activation units) as a Keras Model, with outputs for all the parameters of the Merton Jump Diffusion stochastic process (sigma, mu, jump_sigma, jump_mu, lambda).
+    
+    .. note:: be sure to order the array of desired output parameters as follows: sigma, mu, jump_sigma, jump_mu, lambda.  Each their own array of desired values for every simulated sample path.
+    """
+    
+    input_1 = Input(shape = (40, 50, 1))
+
+    layer1 = Conv2D(32, (12, 12), padding='same', activation='elu')(input_1)
+    layer2 = Conv2D(32, (12, 12), padding='same', activation='elu')(layer1)
+    layer3 = MaxPooling2D(pool_size=(2,2))(layer2)
+
+    layer4 = Conv2D(64, (6, 6), padding='same', activation='elu')(layer3)
+    layer5 = Conv2D(64, (6, 6), padding='same', activation='elu')(layer4)
+    layer6 = MaxPooling2D(pool_size=(2,2))(layer5)
+
+    layer7 = Conv2D(128, (3, 3), padding='same', activation='elu')(layer6)
+    layer8 = Conv2D(128, (3, 3), padding='same', activation='elu')(layer7)
+    layer9 = MaxPooling2D(pool_size=(2,2))(layer8)
+
+    flatten = Flatten()(layer9)
+    
+    output1_layer1 = Dense(32, activation='elu')(flatten)
+    output1_layer2 = Dense(16, activation='elu')(output1_layer1)
+    output1 = Dense(1, name="sigma")(output1_layer2)
+    
+    output2_layer1 = Dense(32, activation='elu')(flatten)
+    output2_layer2 = Dense(16, activation='elu')(output2_layer1)
+    output2 = Dense(1, name="mu")(output2_layer2)
+    
+    output3_layer1 = Dense(32, activation='elu')(flatten)
+    output3_layer2 = Dense(16, activation='elu')(output3_layer1)
+    output3 = Dense(1, name="jump_sigma")(output3_layer2)
+    
+    output4_layer1 = Dense(32, activation='elu')(flatten)
+    output4_layer2 = Dense(16, activation='elu')(output4_layer1)
+    output4 = Dense(1, name="jump_mu")(output4_layer2)
+    
+    output5_layer1 = Dense(32, activation='elu')(flatten)
+    output5_layer2 = Dense(16, activation='elu')(output5_layer1)
+    output5 = Dense(1, name="lambda")(output5_layer2)
+
+    convnet_mo_elu = Model(input = input_1, output=[output1, output2, output3, output4, output5])
+
+    if r_squared:
+        convnet_mo_elu.compile('adam', 'mean_squared_error', metrics=['mean_absolute_percentage_error', r2])
+    else:
+        convnet_mo_elu.compile('adam', 'mean_squared_error', metrics=['mean_absolute_percentage_error'])
+    
+    return convnet_mo_elu
+
 def covnet_single_ReLUs_6_layers():
     """
     returns a 6-layer convolutional architecture (with ReLU activation units) as a Keras Model, with outputs for only a single parameter.
